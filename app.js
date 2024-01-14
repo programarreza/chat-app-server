df1eba01ff61ceab4ce26bcd5749d7761d418a91
programarreza <programarreza@gmail.com>
2024-01-14 20:41:50 +0600

    get all users

--- app.js
+++ app.js
@@ -72,7 +72,25 @@ app.get("/api/conversation/:userId", async (req, res) => {
 // send message
 app.post("/api/message", async (req, res) => {
   try {
-    const { conversationId, senderId, message } = req.body;
+    const { conversationId, senderId, message, receiverId= '' } = req.body;
+    if (!senderId || !message) return res.status(400).send("Please Fill all required fields");
+
+    if (!conversationId && receiverId) {
+      const newConversation = new Conversations({
+        members: [senderId, receiverId],
+      });
+      await newConversation.save();
+      const newMessage = new Messages({
+        conversationId: newConversation._id,
+        senderId,
+        message,
+      });
+      await newMessage.save()
+      return res.status(200).send("Message send Successfully ")
+    }else if(!conversationId && !receiverId){
+      return res.status(400).send('Please fill all require fields')
+    }
+    
     const newMessage = new Messages({ conversationId, senderId, message });
     await newMessage.save();
     res.status(200).send("Message send Successfully");
@@ -85,6 +103,7 @@ app.post("/api/message", async (req, res) => {
 app.get("/api/message/:conversationId", async (req, res) => {
   try {
     const conversationId = req.params.conversationId;
+    if (conversationId === 'new') return res.status(200).json([]);
     const messages = await Messages.find({ conversationId });
     const messageUserData = Promise.all(
       messages.map(async (message) => {
@@ -93,10 +112,29 @@ app.get("/api/message/:conversationId", async (req, res) => {
           user: { email: user.email, fullName: user.fullName },
           message: message.message,
         };
-      }));
+      })
+    );
 
-      res.status(200).json(await messageUserData)
+    res.status(200).json(await messageUserData);
   } catch (error) {}
+});
+
+// get all users
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await Users.find();
+    const usersData = Promise.all(
+      users.map(async (user) => {
+        return {
+          user: { email: user.email, fullName: user.fullName },
+          userId: user._id,
+        };
+      })
+    );
+    res.status(200).json(await usersData);
+  } catch (error) {
+    console.log("error", error);
+  }
 });
 
 app.listen(port, () => {
