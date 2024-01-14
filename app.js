fd85324b9c1dcd74d52d6491d0fe6ad0d478b4bf
programarreza <programarreza@gmail.com>
2024-01-14 17:47:05 +0600

    create post request user-info and conversation

--- app.js
+++ app.js
@@ -2,17 +2,45 @@ const express = require("express");
 const port = process.env.PORT || 5000;
 const app = express();
 
-// import mongoose file
+// connect database
 require("./db/connection");
+
+// import schema file
+const Users = require("./models/Users");
+const Conversations = require("./models/Conversations");
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-
+// routes
 app.get("/", (req, res) => {
   res.send("chat app server is running!");
+});
+
+// user info
+app.post("/api/users", async (req, res) => {
+  try {
+    const { fullName, email } = req.body;
+    const newUser = new Users({ fullName, email });
+    newUser.save();
+    res.send(newUser);
+  } catch (error) {
+    console.log(error, "Error");
+  }
+});
+
+app.post("/api/conversation", async (req, res) => {
+  try {
+    const { senderId, receiverId } = req.body;
+    const newConversation = new Conversations({
+      members: [senderId, receiverId],
+    });
+    await newConversation.save();
+    res.status(200).send('Conversation created successfully')
+  } catch (error) {
+    console.log(error, "error");
+  }
 });
 
 app.listen(port, () => {
---
+++ models/Users.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const Users = mongoose.model("User", userSchema);
+module.exports = Users;
